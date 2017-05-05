bfd6ca912c822310c4269a0c6a8ac2152b77c6d3
Spec-Chum <specchumster@gmail.com>
2017-05-05 15:16:46 +0100

    Formatting - convert spaces to tabs (I may change back later lol)

--- stars.asm
+++ stars.asm
@@ -1,164 +1,164 @@
 ; pasmo --tapbas stars.asm stars.tap
 
-            org 32768
+			org 32768
 
-NUMSTARS    EQU 50
-NUMLAYERS   EQU 4
-SCREEN      EQU $4000
+NUMSTARS	EQU 50
+NUMLAYERS	EQU 4
+SCREEN		EQU $4000
 
-start:      xor     a               ; set up and clear screen to black
-            out     (254), a
-            ld      hl, SCREEN
-            ld      de, SCREEN+1
-            ld      bc, 6144
-            ld      (hl), a
-            ldir
-            
-            ld      bc, 768
-            ld      a, 7            ; set PAPER 0 and INK 7
-            ld      (hl), a
-            ldir
-            
-            ld      hl, 23560       ; reset LAST-K to 0
-            ld      (hl), 0
-            
-init:       ld      b, NUMSTARS     ; loop counter
-            ld      ix, STARS       ; ix = speed, x, y
-            
-firstloop:  push    bc
+start:		xor		a				; set up and clear screen to black
+			out		(254), a
+			ld		hl, SCREEN
+			ld		de, SCREEN+1
+			ld		bc, 6144
+			ld		(hl), a
+			ldir
+			
+			ld		bc, 768
+			ld		a, 7			; set PAPER 0 and INK 7
+			ld		(hl), a
+			ldir
+			
+			ld		hl, 23560		; reset LAST-K to 0
+			ld		(hl), 0
+			
+init:		ld		b, NUMSTARS		; loop counter
+			ld		ix, STARS		; ix = speed, x, y
+			
+firstloop:	push	bc
 
-layer:      call    rand
-            cp      NUMLAYERS
-            jr      nc, layer
-            inc     a               ; make sure speed isn't 0
-            ld      (ix), a         ; store speed
-            ld      e, a
-            
-            call    rand
-            ld      (ix+1), a       ; x can be any byte, so just store
-            ld      b, a
-            ld      d, a            ; cache x pos
-            
-ypos:       call    rand
-            cp      192
-            jr      nc, ypos
-            ld      (ix+2), a       ; store y
-            ld      c, a
-            
-            call    plot
+layer:		call	rand
+			cp		NUMLAYERS
+			jr		nc, layer
+			inc		a				; make sure speed isn't 0
+			ld		(ix), a			; store speed
+			ld		e, a
+			
+			call	rand
+			ld		(ix+1), a		; x can be any byte, so just store
+			ld		b, a
+			ld		d, a			; cache x pos
+			
+ypos:		call	rand
+			cp		192
+			jr		nc, ypos
+			ld		(ix+2), a		; store y
+			ld		c, a
+			
+			call	plot
  
-            ld      a, d
-            sub     e               ; sub speed - looping back to 255 is fine
-            ld      (ix+1), a       ; store new x pos
-           
-            inc     ix
-            inc     ix
-            inc     ix
+			ld		a, d
+			sub		e				; sub speed - looping back to 255 is fine
+			ld		(ix+1), a		; store new x pos
+		   
+			inc		ix
+			inc		ix
+			inc		ix
 
-            pop     bc
-            djnz    firstloop
-                             
-movestars:  ld      a, 5            ; put border to cyan
-            out     (254), a        ; write to port 254
-            
-            ld      b, NUMSTARS     ; loop counter
-            ld      ix, STARS
-            
-mainloop:   push    bc
-            
-            ld      e, (ix)         ; get speed
-            ld      b, (ix+1)       ; get x pos
-            ld      c, (ix+2)       ; get y pos            
-            ld      d, b            ; cache x pos
-                        
-            call    plot
-                        
-            ld      a, d            ; reload x pos
-            add     a, e            ; previous x pos
-            ld      b, a            ; plot only clobbers b, so c is still y pos
-            
-            call    plot
+			pop		bc
+			djnz	firstloop
+							 
+movestars:	ld		a, 5			; put border to cyan
+			out		(254), a		; write to port 254
+			
+			ld		b, NUMSTARS		; loop counter
+			ld		ix, STARS
+			
+mainloop:	push	bc
+			
+			ld		e, (ix)			; get speed
+			ld		b, (ix+1)		; get x pos
+			ld		c, (ix+2)		; get y pos			   
+			ld		d, b			; cache x pos
+						
+			call	plot
+						
+			ld		a, d			; reload x pos
+			add		a, e			; previous x pos
+			ld		b, a			; plot only clobbers b, so c is still y pos
+			
+			call	plot
 
-            ld      a, d
-            sub     e               ; sub speed - looping back to 255 is fine
-            ld      (ix+1), a       ; store new x pos
-           
-            inc     ix
-            inc     ix
-            inc     ix
+			ld		a, d
+			sub		e				; sub speed - looping back to 255 is fine
+			ld		(ix+1), a		; store new x pos
+		   
+			inc		ix
+			inc		ix
+			inc		ix
 
-            pop     bc
-            djnz    mainloop
-            
-            xor     a
-            out     (254), a
-            
-            halt
-            
-            ld      a, (23560)      ; test for keypress
-            or      a            
-            
-            jr      z, movestars    ; loop if no key pressed
+			pop		bc
+			djnz	mainloop
+			
+			xor		a
+			out		(254), a
+			
+			halt
+			
+			ld		a, (23560)		; test for keypress
+			or		a			 
+			
+			jr		z, movestars	; loop if no key pressed
 
-            ret                     ; return to BASIC        
-            
-plot:       ld      a, c            ; IN: B = X, C = Y OUT: HL = address
-            and     7
-            ld      h, a
-            
-            ld      a, c
-            rrca
-            rrca
-            rrca
-            and     $18
-            or      h
-            or      $40
-            ld      h, a
-            
-            ld      a, b
-            rrca
-            rrca
-            rrca
-            and     $1f
-            ld      l, a
-            
-            ld      a, c
-            rlca
-            rlca
-            and     $e0
-            or      l
-            ld      l, a
-            
-            ld      a, b
-            and     7
-                 
-            ld      b, a
-            inc     b
-            ld      a, 254
-            
-rotate:     rrca
-            djnz    rotate
+			ret						; return to BASIC		 
+			
+plot:		ld		a, c			; IN: B = X, C = Y OUT: HL = address
+			and		7
+			ld		h, a
+			
+			ld		a, c
+			rrca
+			rrca
+			rrca
+			and		$18
+			or		h
+			or		$40
+			ld		h, a
+			
+			ld		a, b
+			rrca
+			rrca
+			rrca
+			and		$1f
+			ld		l, a
+			
+			ld		a, c
+			rlca
+			rlca
+			and		$e0
+			or		l
+			ld		l, a
+			
+			ld		a, b
+			and		7
+				 
+			ld		b, a
+			inc		b
+			ld		a, 254
+			
+rotate:		rrca
+			djnz	rotate
 			cpl
-            ld      b, a
-            ld      a, (hl)
-            
-            xor     b
-            ld      (hl), a
+			ld		b, a
+			ld		a, (hl)
+			
+			xor		b
+			ld		(hl), a
 
-            ret
-            
-rand:       ld      hl, (seed)
-            ld      a, h
-            and     $3f             ; make sure we don't get RAM location
-            ld      h, a
-            ld      a, (hl)         ; put result in a
-            inc     hl
-            ld      (seed), hl
-            
-            ret
-            
-seed:       dw      0
-            
-STARS:      ds      NUMSTARS * 3    ; NUMSTARS * SPEED + STAR.X + STAR.Y
-            
+			ret
+			
+rand:		ld		hl, (seed)
+			ld		a, h
+			and		$3f				; make sure we don't get RAM location
+			ld		h, a
+			ld		a, (hl)			; put result in a
+			inc		hl
+			ld		(seed), hl
+			
+			ret
+			
+seed:		dw		0
+			
+STARS:		ds		NUMSTARS * 3	; NUMSTARS * SPEED + STAR.X + STAR.Y
+			
 end start
